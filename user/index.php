7210bab26e9a4f22add17f8b11512c9e77498929
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2014-12-18 21:20:22 -0500

    Cambios en diseño de user
    search
    imagen de usuario
    tarjetas

--- user/index.php
+++ user/index.php
@@ -21,7 +21,7 @@ session_start();
 				<div class="wrap center-block">
 					<div class="contenedor">
 						<figure class="logo left isotipo">
-							<img src="../img/logo.png" alt="Usuario" width="40" height="40">
+							<img src="../img/logo.png" alt="Usuario">
 						</figure>
 						<div class="user right">
 							<span class="notificaciones icon-eye eye-icon left" id="notificaciones"></span>
@@ -37,7 +37,7 @@ session_start();
 			</header>
 
 			<section class="contenido wrap center-block relative">
-				<nav id="menu"class="menu_nav">
+				<nav id="menu"class="menu_nav left">
 								<ul class="menu_ul">
 									<li class="item"><a href="">Perfil</a></li>
 									<li class="item"><a href="../includes/closeconexion.php">Cerrar Sesión</a></li>
@@ -47,11 +47,59 @@ session_start();
 					<ul class="menu2_ul">
 						<li class="menu2-item"><a href="" class="active">Inicio</a></li>
 						<li class="menu2-item"><a href="">Inventario</a></li>
+						<li class="menu2-item"><a href="">Historial</a></li>
 						<li class="menu2-item"><a href="">Soporte</a></li>
 						<li class="menu2-item"><a href="">Politicas</a></li>
-						<li class="menu2-item"><a href="">Otra</a></li>
 					</ul>
-				</nav>			
+				</nav>
+				<div class="contenido-cartas">
+					<form action="" class="searching">
+						<input type="text" name="search" placeholder="Buscar Usuario ..." class="search" />
+						<div class="searchicon">
+							<span class="icon-search search-icon"></span>
+						</div>
+					</form>
+					<div class="contenedorusuario">
+						<div class="avatar left">
+							<h3>Usuario</h3>
+							<figure class="img-avatar round">
+								<img src="img_user/user.jpg" alt="user" width="200" height="200">
+							</figure>
+							<h4>usuario@mail.com</h4>
+							<h4>+7143170</h4>
+
+						</div>
+						<div class="cartas right">
+							<div class="carta-item">
+								<figure>
+									<img src="Tarjetas/draegg.png" alt="" width="200" height="200">
+								</figure>
+							</div>
+							<div class="carta-item">
+								<figure>
+									<img src="Tarjetas/draegg.png" alt="" width="200" height="200">
+								</figure>
+							</div>
+							<div class="carta-item">
+								<figure>
+									<img src="Tarjetas/draegg.png" alt="" width="200" height="200">
+								</figure>
+							</div>
+							<div class="carta-item">
+								<figure>
+									<img src="Tarjetas/draegg.png" alt="" width="200" height="200">
+								</figure>
+							</div>
+							<div class="carta-item">
+								<figure>
+									<img src="Tarjetas/draegg.png" alt="" width="200" height="200">
+								</figure>
+							</div>
+							<div class="comprar"></div>
+						</div>
+					</div>
+
+				</div>
 			</section>
 		</div>	
 	</div>
