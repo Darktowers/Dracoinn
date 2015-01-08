7b442364bfaff8d9d0523270ff12ad44eb77fcbb
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-01-07 21:28:14 -0500

    Que mierda

--- user/php/cambioFoto.php
+++ user/php/cambioFoto.php
@@ -10,7 +10,8 @@ $usuario=$_POST['usuario'];
 $nombreArchivo=$dia.$mes.$anio.$usuario.$_FILES['foto']['name'];
 $telefono=$_POST['telefono'];
 
-if(!empty($tipo)) {
+if($tipo) {
+
 if($tipo=='tipo/jpeg' || $tipo=='image/png' || $tipo=='image/jpg'){
 	if($tamaño>=2000){
 		$destinoReal="../../img_user/";
