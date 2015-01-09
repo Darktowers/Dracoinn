7ac616d0588dddc59d1facc25a76087d7e13e57f
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-01-08 20:39:17 -0500

    Mas validaciones
    Validaciones en cambio de datos
    borrado de imagenes

--- admon/php/cambioFoto.php
+++ admon/php/cambioFoto.php
@@ -12,13 +12,38 @@ $telefono=$_POST['telefono'];
 
 if(!empty($tipo)) {
 if($tipo=='image/jpeg' || $tipo=='image/png' || $tipo=='image/jpg'){
-	if($tamaño>=2000){
+	if($tamaño<=250000){
 		$destinoReal="../../img_user/";
 		$destino="img_user/";
 		$guardado=move_uploaded_file($_FILES['foto']['tmp_name'],$destinoReal.$nombreArchivo);
 		include_once '../../includes/conexion.php';
+		$resul=mysql_query("SELECT * FROM `usuario` WHERE `nickName`=_utf8 '$usuario' collate utf8_bin");
+							  if($row=mysql_fetch_array($resul))
+							  {
+										  do{
+										
+												
+												$fotoBusqueda=stripslashes($row["urlFotoUsuario"]);
+
+										
+										}while($row=mysql_fetch_array($resul));
+							  }	
+
+     $fotoborrar = "../".$fotoBusqueda;
+
+		$queryCorreoy=mysql_query("select correo from usuario where  correo='".$email."'");
+		$queryCorreo=mysql_query("select * from usuario where nickName='".$usuario."' and correo='".$email."'");
+	
+		$confirmacionCorreo=mysql_num_rows($queryCorreo);
+		$confirmacionCorreo2=mysql_num_rows($queryCorreoy);
 		
-				$consulta="update usuario set correo='".$email."', telefono='".$telefono."', urlFotoUsuario='../".$destino.$nombreArchivo."' where nickName='".$usuario."'";
+		
+		   if($confirmacionCorreo==1){
+
+		   		
+
+				unlink($fotoborrar);
+				$consulta="update usuario set correo='".$email."', telefono='".$telefono."', urlFotoUsuario='../".$destino.$nombreArchivo."' where nickName=_utf8'".$usuario."' collate utf8_bin";
 				$query=mysql_query($consulta,$conexion);
 				if($query){
 					header("location:../perfil.php");
@@ -26,8 +51,38 @@ if($tipo=='image/jpeg' || $tipo=='image/png' || $tipo=='image/jpg'){
 					else{
 						echo"<h1 style='color:red'>Error</h1>";
 						}	
+
+
+					}elseif($confirmacionCorreo2 == 0 && $confirmacionCorreo == 0){
+
+								
+
+								unlink($fotoborrar);
+								$consulta="update usuario set correo='".$email."', telefono='".$telefono."', urlFotoUsuario='../".$destino.$nombreArchivo."' where nickName=_utf8'".$usuario."' collate utf8_bin";
+												$query=mysql_query($consulta,$conexion);
+												if($query){
+													header("location:../perfil.php");
+													}
+													else{
+														echo"<h1 style='color:red'>Error</h1>";
+														}	
+					}
+					else{
+						
+
+?>
+			<script> 
+				alert("El correo ya existe");
+				document.location.href="../perfil.php";
+			</script> 
+		<?php
+					}
+				
 		}
-		else{?>
+		else{
+
+echo"$tamaño";
+			?>
 			<script> 
 				alert("La imagen es muy pesada");
 				document.location.href="../perfil.php";
@@ -43,7 +98,18 @@ else{?>
 }
 else{
 include_once '../../includes/conexion.php';
-			$consulta="update usuario set correo='".$email."', telefono='".$telefono."' where nickName='".$usuario."'";
+			$queryCorreoy=mysql_query("select correo from usuario where  correo='".$email."'");
+		$queryCorreo=mysql_query("select correo from usuario where nickName='".$usuario."' and correo='".$email."'");
+	
+		$confirmacionCorreo=mysql_num_rows($queryCorreo);
+		$confirmacionCorreo2=mysql_num_rows($queryCorreoy);
+		
+		
+		   if($confirmacionCorreo==1){
+
+
+		
+				$consulta="update usuario set correo='".$email."', telefono='".$telefono."' where nickName=_utf8'".$usuario."' collate utf8_bin";
 				$query=mysql_query($consulta,$conexion);
 				if($query){
 					header("location:../perfil.php");
@@ -51,5 +117,28 @@ include_once '../../includes/conexion.php';
 					else{
 						echo"<h1 style='color:red'>Error</h1>";
 						}	
+
+
+					}elseif($confirmacionCorreo2==0){
+						
+								$consulta="update usuario set correo='".$email."', telefono='".$telefono."' where nickName=_utf8'".$usuario."' collate utf8_bin";
+												$query=mysql_query($consulta,$conexion);
+												if($query){
+													header("location:../perfil.php");
+													}
+													else{
+														echo"<h1 style='color:red'>Error</h1>";
+														}	
+					}
+					else{
+
+?>
+			<script> 
+				alert("El correo ya existe");
+				document.location.href="../perfil.php";
+			</script> 
+		<?php
+					}
+				
 }
 ?>
