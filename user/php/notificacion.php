7e179a2ce60808bd74215718cb8cc929698a4b55
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-01-09 23:23:16 -0500

    Mamada

--- user/php/notificacion.php
+++ user/php/notificacion.php
@@ -3,7 +3,7 @@ if($_POST)
 	{
 		include_once '../../includes/conexion.php';
         $usuario=$_POST['usuario'];
-    	$consulta=mysql_query("select * from notificaciones where fkUsuarioRemitente =_utf8'".$usuario."' and fkEstadoNotificacion = '111' LIMIT 5 collate utf8_bin");
+    	$consulta=mysql_query("select * from notificaciones where fkUsuarioRemitente ='".$usuario."' and fkEstadoNotificacion = '111' LIMIT 5  ");
 
     	for($i=0;$i=$resultado=mysql_fetch_array($consulta);$i++)
 				{
